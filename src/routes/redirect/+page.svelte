3832a87e3bb1bb64b436ec8077ecc86e9f5b36a6
BlauFx <blaufxsup@gmail.com>
2024-09-23 00:45:47 +0200

    Update redirect
    See https://stackoverflow.com/a/77492119 why onMount is necessary

--- src/routes/redirect/+page.svelte
+++ src/routes/redirect/+page.svelte
@@ -1,33 +1,43 @@
 <script lang="ts">
     import { page } from '$app/stores';
     import { browser } from '$app/environment';
+    import { onMount } from 'svelte';
 
 	let info = [
 		{text: "ask", url: "https://dontasktoask.com"},
 		{text: "hello", url: "https://nohello.net/en/"},
 		{text: "try", url: "https://tryitands.ee"}]
 
-	/*const param = $page.url.searchParams;
-	const str = param.toString().slice(0, -1);
-	for (const element in info) {
-		if (info[element].text == str) {
-			if (browser) {
-			    window.location.href = info[element].url;
+	function sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	async function wait(ms, access) {
+		await sleep(ms);
+		window.location.href = access.url;
+	}
+
+	onMount(() => {
+		const param = $page.url.searchParams;
+		const str = param.toString().slice(0, -1);
+		for (const element in info) {
+			if (info[element].text == str) {
+				if (browser) {
+					wait(2000, info[element]);
+				}
+				break;
 			}
-			break;
 		}
-	}*/
-
-	/*
-
-
-{#each info as {text, url}, _}
-    {text}: <a href={text}>{url}</a>
-    <br>
-{/each}
-	*/
+	})
 
 </script>
+
+{#each info as {text, url}, _}
+    {text}: <a href={url}>{url}</a>
+    <br>
+{/each}
+
+
 
 <p>I will list here some useful links for interacting with people. In case I get the feeling someone wastes my or even their own time with useless stuff. </p>
 
